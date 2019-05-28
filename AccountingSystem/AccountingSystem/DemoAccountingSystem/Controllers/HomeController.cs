98eac8c422bafb74f2bddb3a906bf28377efbb44
Arsenij Aniščanka <BatsilaBox3@gmail.com>
2019-05-29 01:07:32 +0300

    Create demo version

--- AccountingSystem/AccountingSystem/DemoAccountingSystem/Controllers/HomeController.cs
+++ AccountingSystem/AccountingSystem/DemoAccountingSystem/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using DemoAccountingSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using DemoAccountingSystem.Data;
+using DemoAccountingSystem.Data.Entities;
 
 namespace DemoAccountingSystem.Controllers
 {
@@ -39,7 +40,57 @@ namespace DemoAccountingSystem.Controllers
         [Authorize]
         public IActionResult CreateEmployeeList()
         {
-            return View();
+            var employees = _dbContext.Employees.ToList();
+
+            var remoteEmployess = _dbContext.RemoteEmployees
+                .Where(re => !employees.Where(e => e.FirstName == re.FirstName &&
+                e.LastName == re.LastName).Any()).ToList();
+
+            return View(new CreateEmployListViewModel { RemoteEmployees = remoteEmployess });
+        }
+
+        [Authorize]
+        public IActionResult CreateEmployee(int id)
+        {
+            var remoteEmployee = _dbContext.RemoteEmployees.First(re => re.Id == id);
+
+            var viewModel = new CreateEmployeeViewModel
+            {
+                FirstName = remoteEmployee.FirstName,
+                LastName = remoteEmployee.LastName,
+                Position = remoteEmployee.Position
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult CreateEmployee(CreateEmployeeViewModel model)
+        {
+            var employee = new Employee
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Position = model.Position
+            };
+
+            using (var dbContextTransaction = _dbContext.Database.BeginTransaction())
+            {
+                _dbContext.Employees.Add(employee);
+
+                _dbContext.SaveChanges();
+
+                dbContextTransaction.Commit();
+            }
+
+            var employees = _dbContext.Employees.ToList();
+
+            var remoteEmployess = _dbContext.RemoteEmployees
+                .Where(re => !employees.Where(e => e.FirstName == re.FirstName &&
+                e.LastName == re.LastName).Any()).ToList();
+
+            return View("CreateEmployeeList", new CreateEmployListViewModel { RemoteEmployees = remoteEmployess });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ AccountingSystem/AccountingSystem/DemoAccountingSystem/Models/CreateEmployeeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAccountingSystem.Models
+{
+    public class CreateEmployeeViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Position { get; set; }
+    }
+}
