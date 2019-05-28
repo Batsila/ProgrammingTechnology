30b3745dc2717e0ebb58b1a935597184dbf052a5
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2019-05-28 10:40:00 +0300

    Add libs

---
+++ AccountingSystem/AccountingSystem/DemoAccountingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoAccountingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
